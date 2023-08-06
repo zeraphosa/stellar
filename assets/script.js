a7d8af265ac7d9e4a1c31c277aca09c87138b84a
zeraphosa <zaursharifov@outlook.com>
2023-08-06 10:43:07 +0400

    project-overlay script

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,15 @@
+// MENU BUTTON TOGGLE
 const menubtn = document.getElementById("menubtn");
 const menu = document.getElementById("menu");
 
 menubtn.addEventListener("click", () => {
   menu.classList.toggle("mb-menu");
 });
+
+// PROJECT OVERLAY SIZE
+const projectoverlay = document.getElementById("projectoverlay");
+function changeProjectOverlay() {
+  projectoverlay.classList.toggle("project-overlay-mob", window.innerWidth <= 768);
+}
+changeProjectOverlay();
+window.addEventListener("resize", changeProjectOverlay);
