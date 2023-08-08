c0f75bc1d653d6f9bb066d96c7e2b2cea7d55e7d
zeraphosa <zaursharifov@outlook.com>
2023-08-08 20:35:57 +0400

    testimonial slider script done

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,34 @@ function changeProjectOverlay() {
 }
 changeProjectOverlay();
 window.addEventListener("resize", changeProjectOverlay);
+
+// TESTIMONIAL SLIDER
+const testimonialtext = document.getElementById("testimonialtext");
+const testimonialusername = document.getElementById("testimonialusername");
+const testimonialusertitle = document.getElementById("testimonialusertitle");
+const content = [
+  {
+    text: "The best agency we’ve worked with so far. They understand our product and are able to add new features with a great focus.",
+    user: "Jenny Wilson",
+    title: "Vice President",
+  },
+  {
+    text: "Euismod faucibus turpis eu gravida mi. They understand our product and are able to add new features with a great focus.",
+    user: "John Doe",
+    title: "Developer at Microsoft",
+  },
+  {
+    text: "Faucibus turpis eu gravida mi. aliquam sed faucib turpisThey understand our product and are able to add.",
+    user: "Daron William",
+    title: "Specialist",
+  },
+];
+let x = 0;
+setInterval(changetestimonial, 2000);
+function changetestimonial(y = 1) {
+  x += y;
+  if (x > content.length) x = 0;
+  testimonialtext.innerHTML = `"${content[x].text}"`;
+  testimonialusername.innerHTML = `${content[x].user}`;
+  testimonialusertitle.innerHTML = `${content[x].title}`;
+}
